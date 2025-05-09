24241179b6c851fde9ff922211fa28d4f5f69f13
lvxianhao12 <88774330+lvxianhao12@users.noreply.github.com>
2025-05-09 14:24:16 +0800

    Update worker.mjs

--- src/worker.mjs
+++ src/worker.mjs
@@ -139,7 +139,7 @@ async function handleEmbeddings (req, apiKey) {
   return new Response(body, fixCors(response));
 }
 
-const DEFAULT_MODEL = "gemini-2.0-flash";
+const DEFAULT_MODEL = "gemini-2.5-pro-preview-05-06";
 async function handleCompletions (req, apiKey) {
   let model = DEFAULT_MODEL;
   switch (true) {
